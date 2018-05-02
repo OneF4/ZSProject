d7f2e82a9bdbc429faefb5d240ad78ba99b8da59
superGG1990 <superGG1990@163.com>
2018-05-02 22:01:27 +0800

    小框架

---
+++ ZhouShanProject/app/src/main/java/com/example/work/zhoushanproject/base/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.example.work.zhoushanproject.base;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.work.zhoushanproject.R;
+
+/**
+ * Created by work on 2018/5/2.
+ */
+
+public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
