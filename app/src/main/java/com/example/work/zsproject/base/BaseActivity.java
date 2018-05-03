d5c7c76056fc2dfefaa5b8272aa662b60062c998
superGG1990 <superGG1990@163.com>
2018-05-03 08:25:09 +0800

    新

---
+++ app/src/main/java/com/example/work/zsproject/base/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.example.work.zsproject.base;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.example.work.zsproject.R;
+
+/**
+ * Created by work on 2018/5/3.
+ */
+
+public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+
+
+}
