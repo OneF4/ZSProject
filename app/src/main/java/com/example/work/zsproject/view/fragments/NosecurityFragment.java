5640fdd2a15fed39bc18923a351a545ed3e37d05
ayLiuXianSheng <1227522057@qq.com>
2018-05-07 14:34:01 +0800

    舟山

--- app/src/main/java/com/example/work/zsproject/view/fragments/NosecurityFragment.java
+++ app/src/main/java/com/example/work/zsproject/view/fragments/NosecurityFragment.java
@@ -1,28 +1,64 @@
 package com.example.work.zsproject.view.fragments;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.work.zsproject.R;
+import com.example.work.zsproject.view.adapter.NosecurityAdapter;
+import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 未安检页面
  */
 
 public class NosecurityFragment extends Fragment {
+    private View view;
+    private XRecyclerView mNosecurityXrecycleView;
+    private NosecurityAdapter nosecurityAdapter;
+    private List<String> list;
+    /**
+     * 车辆（125）
+     */
+    private TextView mTitleVehicle;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.nosercurity_layout, container, false);
 
+        initView(view);
         return view;
 
+    }
+
+    private void initView(View view) {
+        mNosecurityXrecycleView = (XRecyclerView) view.findViewById(R.id.nosecurity_xrecycleView);
+        mTitleVehicle = (TextView) view.findViewById(R.id.title_vehicle);
+
+        list = new ArrayList<>();
+
+        nosecurityAdapter = new NosecurityAdapter(getContext());
+
+        mNosecurityXrecycleView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        mNosecurityXrecycleView.setAdapter(nosecurityAdapter);
+
+        for (int i = 0; i < 10; i++) {
+            list.add("条目" + i);
+        }
+
+        nosecurityAdapter.setList(list);
+
+        mTitleVehicle.setText("车辆（" + list.size() + "）");
     }
 }
